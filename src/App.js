3e8d045a605f059323ee4c410e4686b07c0f0450
Rachel <80181684+rappleit@users.noreply.github.com>
2023-02-03 16:16:24 +0800

    added title banner

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { useEffect, useState } from 'react';
+import TitleBanner from './components/titleBanner';
+
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TitleBanner />
+      <h2>Hello World</h2>
+      <h2>Hello World</h2>
+      <h2>Hello World</h2>
+      <h2>Hello World</h2>
+      <h2>Hello World</h2>
+      <h2>Hello World</h2>
+      <h2>Hello World</h2>
+      <h2>Hello World</h2>
+      <h2>Hello World</h2>
+      <h2>Hello World</h2>
+      <h2>Hello World</h2>
     </div>
   );
 }
---
+++ src/components/titleBanner.js
@@ -0,0 +1,54 @@
+import { ParallaxBanner, ParallaxBannerLayer } from 'react-scroll-parallax';
+import layer1 from '../assets/mountain1.png'
+import layer2 from '../assets/mountain2.png'
+import layer3 from '../assets/mountain3.png'
+import layer4 from '../assets/mountain4.png'
+
+
+const TitleBanner = () => {
+    return (
+        <div className="titleBanner">
+            <ParallaxBanner style={{ aspectRatio: '2 / 1' }} className="parallaxBanner"
+                layers={[
+                    {
+                        image: layer1,
+                        translateY: [0, 40],
+                        shouldAlwaysCompleteAnimation: true,
+                        expanded: false,
+                    },
+                    {
+                        image: layer2,
+                        translateY: [5, 35],
+
+                        shouldAlwaysCompleteAnimation: true,
+                        expanded: false,
+                    },
+                    {
+                        image: layer3,
+                        translateY: [10, 30],
+
+                        shouldAlwaysCompleteAnimation: true,
+                        expanded: false,
+                    },
+                    {
+                        image: layer4,
+                        translateY: [15, 25],
+
+                        shouldAlwaysCompleteAnimation: true,
+                        expanded: false,
+                    },
+
+                ]}>
+                <ParallaxBannerLayer className="titleLayer"  speed={-15}>
+                    <div className="titleContainer">
+                        <img src="./SEVEN_logo.svg" className="logoImg"/>
+                        <h1>SUTD's Premier Entrepreneurship Network</h1>
+                    </div>
+                </ParallaxBannerLayer>
+            </ParallaxBanner>
+
+        </div>
+    );
+}
+
+export default TitleBanner;
