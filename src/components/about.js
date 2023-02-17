0ebe3120713e6b2b82dbb23affe3b35972f0dc96
Rachel <80181684+rappleit@users.noreply.github.com>
2023-02-18 02:56:04 +0800

    updated about text

--- src/components/about.js
+++ src/components/about.js
@@ -2,7 +2,7 @@ const About = () => {
     return ( 
         <div className="aboutContainer section">
             <h1>What is SEVEN?</h1>
-            <p className="aboutText">SUTD Entrepreneurship, Venture and Enterprise Network (SEVEN) is SUTD’s premier entrepreneurship network, supported both by its Masters of Technology Entrepreneurship programme (STEP) and SUTD’s Venture, Innovation and Entrepreneurship (VIE) office. SEVEN represents the entrepreneurship community within SUTD’s student body, spearheading initiatives to facilitate and develop its vibrant startup ecosystem. It recently launched a new Web3 division, focusing on talent in the emerging Web3 space through growing its analysis and blockchain development capabilities.</p>
+            <p className="aboutText">SEVEN is SUTD’s premier entrepreneurship network, supported both by its Masters of Technology Entrepreneurship programme (STEP) and SUTD’s Venture, Innovation and Entrepreneurship (VIE) office. We represents the entrepreneurship community within SUTD’s student body, spearheading initiatives to facilitate and develop its vibrant startup ecosystem.</p>
             <div className="supportContainer">
                 <img src="./STEP_logo.svg" alt="SUTD Technology Entrepreneurship Programme logo"/>
                 <img src="./SUTD_VIE_Logo.png" alt="SUTD’s Venture, Innovation and Entrepreneurship office logo"/>
