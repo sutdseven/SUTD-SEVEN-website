5f31f877ed881f8365d9fc3843aee912beb6e590
Rachel <80181684+rappleit@users.noreply.github.com>
2023-03-06 18:01:31 +0800

    updated exco css module

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,3 +1,5 @@
+import contactCSS from '../styles/contact.module.css'
+
 const Contact = () => {
     return (
         <div className="contactContainer section">
--- src/components/exco.js
+++ src/components/exco.js
@@ -4,35 +4,35 @@ const Exco = () => {
     return ( 
         <div className={excoCSS.container}>
             <h2>EXCO</h2>
-            <h3 className="batchTitle">2023 - PIONEER</h3>
-            <div className="teamContainer">
-                <div className="teamMember">
-                    <img src="./excoPlaceholder.png" className="excoImg" alt=""/>
+            <h3>2023 - PIONEER</h3>
+            <div className={excoCSS.memberContainer}>
+                <div className={excoCSS.member}>
+                    <img src="./excoPlaceholder.png" alt=""/>
                     <h3>Aaron Tua</h3>
                     <p>President</p>
                 </div>
-                <div className="teamMember">
-                    <img src="./excoPlaceholder.png" className="excoImg" alt=""/>
+                <div className={excoCSS.member}>
+                    <img src="./excoPlaceholder.png" alt=""/>
                     <h3>Lee Yan Han</h3>
                     <p>Vice President</p>
                 </div>
-                <div className="teamMember">
-                    <img src="./excoPlaceholder.png" className="excoImg" alt=""/>
+                <div className={excoCSS.member}>
+                    <img src="./excoPlaceholder.png" alt=""/>
                     <h3>Justin Wong</h3>
                     <p>Head Of Outreach</p>
                 </div>
-                <div className="teamMember">
-                    <img src="./excoPlaceholder.png" className="excoImg" alt=""/>
+                <div className={excoCSS.member}>
+                    <img src="./excoPlaceholder.png" alt=""/>
                     <h3>Matthias Lee</h3>
                     <p>Head Of Community</p>
                 </div>
-                <div className="teamMember">
-                    <img src="./excoPlaceholder.png" className="excoImg" alt=""/>
+                <div className={excoCSS.member}>
+                    <img src="./excoPlaceholder.png" alt=""/>
                     <h3>Muthuvel Vimal</h3>
                     <p>Web3 Director</p>
                 </div>
-                <div className="teamMember">
-                    <img src="./excoPlaceholder.png" className="excoImg" alt=""/>
+                <div className={excoCSS.member}>
+                    <img src="./excoPlaceholder.png" alt=""/>
                     <h3>Vivek Thomas</h3>
                     <p>Web3 Director</p>
                 </div>
