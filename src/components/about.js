dc5490d5379a6460003061fa424e7121adc424d6
Rachel <80181684+rappleit@users.noreply.github.com>
2023-03-06 13:50:16 +0800

    added about section css module

--- src/components/about.js
+++ src/components/about.js
@@ -1,9 +1,11 @@
+import AboutCSS from '../styles/about.module.css'
+
 const About = () => {
     return ( 
-        <div className="aboutContainer section">
+        <div className={AboutCSS.container}>
             <h1>What is SEVEN?</h1>
-            <p className="aboutText">SEVEN is SUTD’s premier entrepreneurship network, supported both by its Masters of Technology Entrepreneurship programme (STEP) and SUTD’s Venture, Innovation and Entrepreneurship (VIE) office. We represents the entrepreneurship community within SUTD’s student body, spearheading initiatives to facilitate and develop its vibrant startup ecosystem.</p>
-            <div className="supportContainer">
+            <p className={AboutCSS.text}>SEVEN is SUTD’s premier entrepreneurship network, supported both by its Masters of Technology Entrepreneurship programme (STEP) and SUTD’s Venture, Innovation and Entrepreneurship (VIE) office. We represents the entrepreneurship community within SUTD’s student body, spearheading initiatives to facilitate and develop its vibrant startup ecosystem.</p>
+            <div className={AboutCSS.logoContainer}>
                 <img src="./STEP_logo.svg" alt="SUTD Technology Entrepreneurship Programme logo"/>
                 <img src="./SUTD_VIE_Logo.png" alt="SUTD’s Venture, Innovation and Entrepreneurship office logo"/>
             </div>
